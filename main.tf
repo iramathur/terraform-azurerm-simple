df2d9a0e2a9936f6132bababbe143e991a199f97
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-12-17 15:57:45 +0530

    Update main.tf
    dev branch changes

--- main.tf
+++ main.tf
@@ -14,3 +14,19 @@ resource "azurerm_storage_account" "stor" {
   account_tier             = "${var.storage_account_tier}"
   account_replication_type = "${var.storage_replication_type}"
 }
+
+resource "azurerm_storage_account1" "stor" {
+  name                     = "stor${random_id.server.hex}"
+  location                 = "${var.region}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  account_tier             = "${var.storage_account_tier}"
+  account_replication_type = "${var.storage_replication_type}"
+}
+
+resource "azurerm_storage_account2" "stor" {
+  name                     = "stor${random_id.server.hex}"
+  location                 = "${var.region}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  account_tier             = "${var.storage_account_tier}"
+  account_replication_type = "${var.storage_replication_type}"
+}
